4ae8e42b2b42577d9cb46f7568b9feff367710d8
aollervb <aollervb@gmail.com>
2026-03-17 02:23:31 +0100

    feat: correctly implemented binary search

--- src/main/java/org/example/BTree/BTreeIterative.java
+++ src/main/java/org/example/BTree/BTreeIterative.java
@@ -21,25 +21,14 @@ public class BTreeIterative implements BTree {
         Node currentNode = root;
         while (currentNode != null) {
             if (currentNode.isLeaf()) {
-                if (currentNode.isLeaf()) {
-                    return binarySearch(key, currentNode.getKeys()) != -1;
-                }
+                return contains(key, currentNode.getKeys());
             }
 
-            if (currentNode.getKeys().getLast() < key) {
-                currentNode = currentNode.getChildren().getLast();
-                continue;
+            if (contains(key, currentNode.getKeys())) {
+                return true;
             }
 
-            int idx = binarySearch(key, currentNode.getKeys());
-            if (idx != -1) return true;
-
-            int childIdx = 0;
-            List<Integer> keys = currentNode.getKeys();
-            while (childIdx < keys.size() && key > keys.get(childIdx)) {
-                childIdx++;
-            }
-            currentNode = currentNode.getChildren().get(childIdx);
+            currentNode = currentNode.getChildren().get(lowerBound(key, currentNode.getKeys()));
         }
         return false;
     }
@@ -59,69 +48,38 @@ public class BTreeIterative implements BTree {
         }
 
         while (!keyAlreadyInserted) {
-            if (currentNode.getKeys().size() == 2 * degree - 1) {
-                int middleKey = currentNode.getKeys().get(degree - 1);
+            List<Integer> keys = currentNode.getKeys();
+            if (keys.size() == 2 * degree - 1) {
+                int middleKey = keys.get(degree - 1);
                 if (parent == null) {
-                    splitRoot(currentNode, key);
+                    splitRoot(currentNode);
                     currentNode = root;
                 } else {
-                    splitNode(parent, currentNode, key);
+                    splitNode(parent, currentNode);
                     currentNode = key < middleKey
                             ? parent.getChildren().get(parent.getKeys().indexOf(middleKey))
                             : parent.getChildren().get(parent.getKeys().indexOf(middleKey) + 1);
                 }
             }
+            int idx = lowerBound(key, currentNode.getKeys());
 
             if (currentNode.isLeaf()) {
-                for (int k : currentNode.getKeys()) {
-                    if (k > key) {
-                        currentNode.getKeys().add(currentNode.getKeys().indexOf(k), key);
-                        keyAlreadyInserted = true;
-                        break;
-                    }
-                }
-                if (!keyAlreadyInserted) {
-                    currentNode.getKeys().addLast(key);
-                    keyAlreadyInserted = true;
-                }
+                currentNode.getKeys().add(idx, key);
+                keyAlreadyInserted = true;
                 continue;
             }
-
-            if (currentNode.getKeys().getLast() < key) {
-                parent = currentNode;
-                currentNode = currentNode.getChildren().getLast();
-                continue;
-            }
-
-            if (currentNode.getKeys().getFirst() > key) {
-                parent = currentNode;
-                currentNode = currentNode.getChildren().getFirst();
-                continue;
-            }
-
-            for (int i = 1; i < currentNode.getKeys().size(); i++) {
-                if (key < currentNode.getKeys().get(i)) {
-                    parent = currentNode;
-                    currentNode = currentNode.getChildren().get(i);
-                    break;
-                }
-            }
+            parent = currentNode;
+            currentNode = currentNode.getChildren().get(idx);
         }
         return keyAlreadyInserted;
     }
 
-    private void splitNode(Node parent, Node node, int key) {
+    private void splitNode(Node parent, Node node) {
         // Add the middle key to the parent Node
         int middleKey = node.getKeys().get(degree - 1);
 
-        for (int k : parent.getKeys()) {
-            if (middleKey < k) {
-                parent.getKeys().add(parent.getKeys().indexOf(k), middleKey);
-                break;
-            }
-        }
-
-        if (parent.getKeys().getLast() < middleKey) parent.getKeys().addLast(middleKey);
+        int idx = lowerBound(middleKey, parent.getKeys());
+        parent.getKeys().add(idx, middleKey);
 
         // Create two new Nodes and add keys and children
         Node newLeftNode = new Node(
@@ -135,12 +93,11 @@ public class BTreeIterative implements BTree {
 
         // Remove original Node from the Parent
         parent.getChildren().remove(node);
-
-        parent.getChildren().add(parent.getKeys().indexOf(middleKey), newLeftNode);
-        parent.getChildren().add(parent.getKeys().indexOf(middleKey) + 1, newRightNode);
+        parent.getChildren().add(idx, newLeftNode);
+        parent.getChildren().add(idx + 1, newRightNode);
     }
 
-    private void splitRoot(Node root, int key) {
+    private void splitRoot(Node root) {
         Node newRoot = new Node(
                 new ArrayList<>(),
                 new ArrayList<>()
@@ -165,7 +122,7 @@ public class BTreeIterative implements BTree {
 
     /*
         Binary Search implementation.  Returns index of the key in the keys list
-        If output is -1, the number was not found.
+        If the element isn't present on the list it returns the idx where it should be inserted
      */
     private int binarySearch(int key, List<Integer> keys) {
         int low = 0, high = keys.size() - 1;
@@ -181,6 +138,27 @@ public class BTreeIterative implements BTree {
                 high = mid - 1;
             }
         }
-        return -1;
+        return low;
+    }
+
+    private boolean contains(int key, List<Integer> keys) {
+        int low = 0, high = keys.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (keys.get(mid) == key) return true;
+            if (keys.get(mid) < key) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+
+    private int lowerBound(int key, List<Integer> keys) {
+        int low = 0, high = keys.size();
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (keys.get(mid) < key) low = mid + 1;
+            else high = mid;
+        }
+        return low;
     }
 }
